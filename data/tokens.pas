461c87a4934679957c1734629e635176c76a9282
Talon Daniels <talondaniels@gmail.com>
2013-01-31 13:08:07 -0700

    implemented invalid character MP_ERROR

--- data/tokens.pas
+++ data/tokens.pas
@@ -1,11 +1,12 @@
 'kugj''xdgsgds' 'gabad'
    'string one                  
                       
-                      
+ ?                     
 integer and div ) 'asdf'
 .,;()=+-*
 ::= < <>  <=  >  >=
 
+   [
 3234.234 to thisIsAVarName
 7
 9e6
