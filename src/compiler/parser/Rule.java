9842737e99ea0417fadb9f05a64c1f762afc727c
Chad Marmon <Chad@Monolith.local>
2013-02-07 15:40:06 -0700

    Added Stubs in class Rule from 35 to 43

--- src/compiler/parser/Rule.java
+++ src/compiler/parser/Rule.java
@@ -129,6 +129,42 @@ public class Rule {
             error();
         }
     }
+    
+    public void type()
+    {
+        switch (globalLookAhead.getType())
+        {
+        case MP_INTEGER:
+            match(TokenType.MP_INTEGER);
+            break;
+        case MP_FIXED:
+            match(TokenType.MP_FIXED); //change that to a boolean that we need to create
+            break;
+        case MP_FLOAT:
+            match(TokenType.MP_FLOAT);
+            break;
+        default:
+            error();
+        }
+    }
+
+    public void procedureAndFunctionDeclarationPart()
+    {
+        switch (globalLookAhead.getType()) {
+        case MP_PROCEDURE:
+            procedureDeclaration();
+            procedureAndFunctionDeclarationPart();
+            break;
+        case MP_FUNCTION:
+            functionDeclaration();
+            procedureAndFunctionDeclarationPart();
+            break;
+        case MP_BEGIN:
+            break;
+        default:
+            error();
+        }
+    }
 
     public void procedureDeclaration()
     {
@@ -203,37 +239,130 @@ public class Rule {
             error();
         }
     }
-
-    public void type()
+    
+    //Starting at 35
+    
+    public void ifStatement()
     {
         switch (globalLookAhead.getType())
         {
-        case MP_INTEGER:
-            match(TokenType.MP_INTEGER);
-            break;
-        case MP_FIXED:
-            match(TokenType.MP_FIXED); //change that to a boolean that we need to create
-            break;
-        case MP_FLOAT:
-            match(TokenType.MP_FLOAT);
+        case MP_IF:
+            match(TokenType.MP_IF);
+            booleanExpression();
+            match(TokenType.MP_THEN);
+            statement();
+            optionalElsePart();
             break;
         default:
             error();
         }
     }
-
-    public void procedureAndFunctionDeclarationPart()
+    
+    public void optionalElsePart()
     {
-        switch (globalLookAhead.getType()) {
-        case MP_PROCEDURE:
-            procedureDeclaration();
-            procedureAndFunctionDeclarationPart();
+        switch (globalLookAhead.getType())
+        {
+        case MP_ELSE:
+            match(TokenType.MP_ELSE);
+            statement();
+            match(TokenType.MP_END); //TODO fix this so that the epsilon isn't ambiguous
             break;
-        case MP_FUNCTION:
-            functionDeclaration();
-            procedureAndFunctionDeclarationPart();
+        case MP_UNTIL:
+        case MP_SCOLON:
+        case MP_END:
             break;
-        case MP_BEGIN:
+        default:
+            error();
+        }
+    }
+    
+    public void repeatStatement()
+    {
+        switch (globalLookAhead.getType())
+        {
+        case MP_REPEAT:
+            match(TokenType.MP_REPEAT);
+            statementSequence();
+            match(TokenType.MP_UNTIL);
+            booleanExpression();
+            break;
+        default:
+            error();
+        }
+    }
+    
+    public void whileStatement()
+    {
+        switch (globalLookAhead.getType())
+        {
+        case MP_WHILE:
+            match(TokenType.MP_WHILE);
+            booleanExpression();
+            match(TokenType.MP_DO);
+            statement();
+            break;
+        default:
+            error();
+        }
+    }
+    
+    public void forStatement()
+    {
+        switch (globalLookAhead.getType())
+        {
+        case MP_FOR:
+            match(TokenType.MP_FOR);
+            controlVariable();
+            match(TokenType.MP_ASSIGN);
+            initialValue();
+            stepValue();
+            finalValue();
+            match(TokenType.MP_DO);
+            statement();
+            break;
+        default:
+            error();
+        }
+    }
+    
+    public void controlVariable()
+    {
+        switch (globalLookAhead.getType())
+        {
+        case MP_IDENTIFIER:
+            variableIdentifier();
+            break;
+        default:
+            error();
+        }
+    }
+    
+    public void initialValue()
+    {
+        switch (globalLookAhead.getType())
+        {
+        case MP_IDENTIFIER:
+        case MP_LPAREN:
+        case MP_NOT:
+        case MP_INTEGER_LIT:
+        case MP_MINUS:
+        case MP_PLUS:
+            ordinalExpression();
+            break;
+        default:
+            error();
+        }
+    }
+    
+    public void stepValue()
+    {
+        switch (globalLookAhead.getType())
+        {
+        case MP_TO:
+            match(TokenType.MP_TO);
+            break;
+        case MP_DOWNTO:
+            match(TokenType.MP_DOWNTO);
             break;
         default:
             error();
