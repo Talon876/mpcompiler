6b552d467ef9986538fb1298835ad8c99c167b9b
Chad Marmon <chadthman@gmail.com>
2013-02-06 17:41:10 -0700

    Added the parser package with class file Rule
    not finished yet

---
+++ src/compiler/parser/Rule.java
@@ -0,0 +1,101 @@
+package compiler.parser;
+
+import compiler.Token;
+
+public class Rule {
+
+    Token globalLookAhead;
+//    public void applyRule()
+//    {
+//        
+//        switch(globalLookAhead)
+//        {
+//            case :
+//            default :
+//                error();
+//        }
+//        
+//            
+//    }
+    
+    public void match(Token nonterminal)
+    {
+       
+    }
+    
+    public void systemGoal()
+    {
+        
+    }
+    
+    public void program()
+    {
+        
+    }
+    
+    public void programHeadding()
+    {
+        
+    }
+    
+    public void block()
+    {
+        
+    }
+    
+    public void variableDeclarationPart()
+    {
+        
+    }
+    
+    public void variableDeclarationTail()
+    {
+        
+    }
+    
+    public void variableDeclaration()
+    {
+        
+    }
+    
+    public void procedureDeclaration()
+    {
+        
+    }
+    
+    public void functionDeclaration()
+    {
+        
+    }
+    
+    public void procedureHeading()
+    {
+        
+    }
+    
+    public void fuctionHeadding()
+    {
+        
+    }
+    
+    public void optionalFormalParameterList()
+    {
+        
+    }
+    
+    public void type()
+    {
+        
+    }
+    
+    public void procedureAndFunctionDeclarationPart()
+    {
+        
+    }
+    
+    public void error()
+    {
+        
+    }
+    
+}
